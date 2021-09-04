87eebb282e2a27c07dc71591159bbf726fd807d3
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-09-04 16:47:01 -0400

    removed styled components and build error

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,25 +1,6 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import { NavigationLinkProps } from '../interfaces';
-import styled from 'styled-components';
-
-// const StyledNavLink = styled(NavLink)`
-//     text-decoration: none;
-
-//     &:focus, &:visited, &:link, &:active {
-//         text-decoration: none;
-        
-//     }
-//     color: #707070;
-
-   
-// `;
-
-/*
- &:hover {
-        color: green
-    }
-    */
 
 
 export default function NavigationLink({text, location, size, additionalClassNames}: NavigationLinkProps) {
